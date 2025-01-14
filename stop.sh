f16f4f0756eae8bf2889d30b5c3b15497588d439
Óscar Parro <oscarparro101@hotmail.com>
2025-01-14 13:24:03 +0100

    Nuevas modificaciones

--- stop.sh
+++
@@ -1,8 +0,0 @@
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -q) -f
-docker volume rm $(docker volume ls -q)
-docker network rm $(docker network ls -q | grep -v "bridge\|host\|none")
-docker system prune -a --volumes -f
-
-docker exec -it kafka kafka-console-consumer.sh   --bootstrap-server kafka:9092   --topic flight_delay_classification_response   --from-beginning
